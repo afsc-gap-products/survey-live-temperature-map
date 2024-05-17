9afc4e1ae98017a89ee71910140cbd5c7e004c96
emilymarkowitz-noaa <emily.markowitz@noaa.gov>
2024-05-17 01:20:04 -0700

    typo

--- code/data.R
+++ code/data.R
@@ -49,7 +49,7 @@ if (length(a)>0) { # if there are enteries for this year in the spreadsheet
       b %>% 
         dplyr::filter(!is.na(station))%>%
         dplyr::mutate(
-          gear_temperature_c = as.character(gear_temperature_c), 
+          gear_temperature_c = as.numeric(gear_temperature_c), 
           station = as.character(station), 
                       stratum = as.numeric(stratum)))
   }
